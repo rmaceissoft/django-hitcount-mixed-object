89eaa586d8d7bdc36e5a85bf85a315635ec72d20
Reiner Marquez <rmaceissoft@gmail.com>
2012-02-21 14:41:26 -0500

    added to accesible methods to get/set connection status to NoSQL database.

--- hitcount_mixed_object/backends/__init__.py
+++ hitcount_mixed_object/backends/__init__.py
@@ -8,16 +8,27 @@ class InvalidBackend(Exception):
 
 _connected = False
 
+def is_connected():
+    """ public method to get connection status
+    """
+    global _connected
+    return _connected
+
+
+def mark_as_connected():
+    """ public method to set as True connection status
+    """
+    global _connected
+    _connected = True
+
 def get_backend():
     try:
         backend = import_module(settings.HITCOUNT_MIXED_OBJECT_BACKEND)
     except:
         raise InvalidBackend("Could not load '%s' as a backend" % settings.HITCOUNT_MIXED_OBJECT_BACKEND )
 
-    global _connected
-    if not _connected:
+    if not is_connected():
         backend.init_connection()
-        _connected = True
-
+        mark_as_connected()
 
     return backend
